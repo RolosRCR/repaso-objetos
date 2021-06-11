bbcf0560619e132df6b108b347100aa52d9d2554
RolosRCR <a286847>
2021-06-10 21:36:03 -0500

    Propuesta de solucion ejercicio5 problemas con mockito

--- src/main/java/uaslp/objetos/escuela/EvaluadorDePromedios.java
+++ src/main/java/uaslp/objetos/escuela/EvaluadorDePromedios.java
@@ -3,10 +3,11 @@ package uaslp.objetos.escuela;
 import java.util.List;
 
 public class EvaluadorDePromedios {
-
-    public double evalua(List<Double> calificaciones){
+    private double promedio;
+    public double evalua(List<Double> calificaciones) {
         double sum = calificaciones.stream().reduce(0.0, Double::sum);
+        promedio = sum / calificaciones.size();
 
-        return sum / calificaciones.size();
+        return promedio;
     }
 }
